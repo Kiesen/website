9aa79a2a8196969ae7e8c7efadb967c22d391046
Frederik Maximilian Aulich <frederik.aulich@uni-duesseldorf.de>
2020-07-15 00:08:34 +0200

    Add head title to the index page.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,12 @@
+import Head from 'next/head';
 import { FC } from 'react';
 
 const Home: FC<{}> = () => {
   return (
     <>
+      <Head>
+        <title>Home - Frederik Aulich</title>
+      </Head>
       <div>Frederik Aulich</div>
     </>
   );

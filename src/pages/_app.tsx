3ae0f6ef2dbd579d4846dc805ff96e9a30279156
Frederik Maximilian Aulich <frederik.aulich@uni-duesseldorf.de>
2020-07-16 22:50:21 +0200

    Overwrite default theme and add it to the theme provider.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,9 +2,11 @@ import { AppProps } from 'next/app';
 import { FC } from 'react';
 import { CSSReset, ThemeProvider } from '@chakra-ui/core';
 
+import theme from '@src/styles/theme';
+
 const App: FC<AppProps> = ({ Component, pageProps }) => {
   return (
-    <ThemeProvider>
+    <ThemeProvider theme={theme}>
       <CSSReset />
       <Component {...pageProps} />
     </ThemeProvider>

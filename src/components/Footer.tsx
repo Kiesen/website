b50410ad4c7ba1ef941194b56bb24e64e86d2ebf
Frederik Maximilian Aulich <frederik.aulich@uni-duesseldorf.de>
2021-01-10 14:42:00 +0100

    Add clock to the footer.

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,10 +1,26 @@
-import { FC } from 'react';
+import { FC, useEffect, useState } from 'react';
 
 const Footer: FC<{}> = () => {
-  const date = new Date().getFullYear();
+  const getClockString = (): string => {
+    const date = new Date();
+    const seconds = date.getSeconds();
+    const minutes = date.getMinutes();
+    const hours = date.getHours();
+
+    return `${hours}:${minutes}:${seconds}`;
+  };
+
+  const [time, setTime] = useState(getClockString());
+
+  useEffect(() => {
+    setTimeout(() => {
+      setTime(getClockString());
+    }, 1000);
+  }, [time]);
+
   return (
     <footer className="w-full p-4 text-center animate-glitch">
-      {date}
+      {time}
     </footer>
   );
 };

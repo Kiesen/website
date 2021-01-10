f5ad959471f6ca2a82657f772049f5b7dbd291f0
Frederik Maximilian Aulich <frederik.aulich@uni-duesseldorf.de>
2021-01-10 21:07:31 +0100

    Update how the background image on the landingpage is set.

--- src/components/Background.tsx
+++ src/components/Background.tsx
@@ -4,9 +4,17 @@ type BackgroundProps = {
   name: string;
 };
 
+/**
+ * -- TODO --
+ * Currently it is not possible to set the background image over the tailwindcss
+ * class. Therefore I introduced this temporary workaround. After some research
+ * this should be replaced with the initial implementation.
+ *
+ */
 const Background: FC<BackgroundProps> = ({ children, name }) => (
   <div
     className={`bg-${name} bg-no-repeat bg-cover bg-fixed bg-center`}
+    style={{ backgroundImage: "url('/images/retro.gif')" }}
   >
     {children}
   </div>

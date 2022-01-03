1e55a0dc6ce4f94d97100e24ee9a2ba9894c1e86
Frederik Maximilian Aulich <frederik.aulich@uni-duesseldorf.de>
2022-01-03 02:11:11 +0100

    Update nav bar to a more generic component.

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -4,53 +4,67 @@ import { FaGithub, FaTwitter, FaLinkedinIn } from 'react-icons/fa';
 
 const iconSize = '35px';
 
-const NavBar: FC<{}> = () => (
-  <nav className="w-full container mx-auto">
+type Props = {
+  mainTitle: string;
+  githubLink?: string;
+  linkedInLink?: string;
+  twitterLink?: string;
+};
+
+const NavBar: FC<Props> = ({
+  mainTitle,
+  githubLink,
+  linkedInLink,
+  twitterLink,
+}) => (
+  <nav className="w-full container mx-auto text-white">
     <div className="w-full flex items-center justify-between">
-      <div className="flex items-center text-indigo-400 no-underline hover:no-underline font-bold text-2xl lg:text-4xl">
+      <div className="flex items-center no-underline hover:no-underline font-bold text-2xl lg:text-4xl">
         <Link href="/" passHref>
-          <span className="bg-clip-text text-transparent bg-gradient-to-r from-purple-500 via-pink-300 to-gray-100">
-            frederik aulich
-          </span>
+          <span>{mainTitle}</span>
         </Link>
       </div>
 
       <div className="flex w-1/2 justify-end content-center">
-        <a
-          href={'https://github.com/kiesen'}
-          rel="noreferrer"
-          target="_blank"
-          className="inline-block md:h-auto p-2 md:p-4"
-        >
-          <FaGithub
-            className="transition duration-500 ease-in-out transform hover:scale-110"
-            size={iconSize}
-          />
-        </a>
-        <a
-          href={
-            'https://www.linkedin.com/in/frederik-aulich-08736a171/'
-          }
-          rel="noreferrer"
-          target="_blank"
-          className="inline-block md:h-auto p-2 md:p-4"
-        >
-          <FaLinkedinIn
-            className="transition duration-500 ease-in-out transform hover:scale-110"
-            size={iconSize}
-          />
-        </a>
-        <a
-          href={'https://twitter.com/frederikaulich'}
-          rel="noreferrer"
-          target="_blank"
-          className="inline-block md:h-auto p-2 md:p-4"
-        >
-          <FaTwitter
-            className="transition duration-500 ease-in-out transform hover:scale-110"
-            size={iconSize}
-          />
-        </a>
+        {githubLink ? (
+          <a
+            href={githubLink}
+            rel="noreferrer"
+            target="_blank"
+            className="inline-block md:h-auto p-2 md:p-4"
+          >
+            <FaGithub
+              className="transition duration-500 ease-in-out transform hover:scale-110"
+              size={iconSize}
+            />
+          </a>
+        ) : null}
+        {linkedInLink ? (
+          <a
+            href={linkedInLink}
+            rel="noreferrer"
+            target="_blank"
+            className="inline-block md:h-auto p-2 md:p-4"
+          >
+            <FaLinkedinIn
+              className="transition duration-500 ease-in-out transform hover:scale-110"
+              size={iconSize}
+            />
+          </a>
+        ) : null}
+        {twitterLink ? (
+          <a
+            href={twitterLink}
+            rel="noreferrer"
+            target="_blank"
+            className="inline-block md:h-auto p-2 md:p-4"
+          >
+            <FaTwitter
+              className="transition duration-500 ease-in-out transform hover:scale-110"
+              size={iconSize}
+            />
+          </a>
+        ) : null}
       </div>
     </div>
   </nav>

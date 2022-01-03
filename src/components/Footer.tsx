32a5352917ea08ee23cf295216b37fd3caf3ee18
Frederik Maximilian Aulich <frederik.aulich@uni-duesseldorf.de>
2022-01-03 01:44:18 +0100

    Update footer component.

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,20 @@
 import { FC } from 'react';
+import { FaCuttlefish } from 'react-icons/fa';
 
-const Footer: FC<{}> = () => {
+type Props = {
+  textContent: string;
+  showCurrentYear: boolean;
+};
+
+const Footer: FC<Props> = ({ textContent, showCurrentYear }) => {
+  const currentYear = new Date().getUTCFullYear();
   return (
-    <footer className="w-full pt-16 pb-6 text-sm text-center fade-in"></footer>
+    <footer className="w-full pt-16 pb-6 text-sm text-center fade-in">
+      <p className="text-gray-500 no-underline hover:no-underline">
+        &copy; {showCurrentYear ? `${currentYear + ' '}` : ''}
+        {textContent}
+      </p>
+    </footer>
   );
 };
 

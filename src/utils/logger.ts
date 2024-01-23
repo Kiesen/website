083185091570ce3201585a71112068219b9882df
Frederik Aulich <frederik.aulich@trivago.com>
2024-01-23 02:40:45 +0100

    Add logger prefix.

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -6,13 +6,16 @@ const error = console.error;
 const logger = (ns: string) => ({
   stdout: (message: string, optionalParams?: any) => {
     optionalParams
-      ? log(chalk.bold(`${ns}: ${message}`), optionalParams)
-      : log(chalk.bold(`${ns}: ${message}`));
+      ? log(chalk.bold(`[ok]:${ns}: ${message}`), optionalParams)
+      : log(chalk.bold(`[ok]:${ns}: ${message}`));
   },
   stderr: (message: string, optionalParams?: any) => {
     optionalParams
-      ? error(chalk.red.bold(`${ns}: ${message}`), optionalParams)
-      : error(chalk.red.bold(`${ns}: ${message}`));
+      ? error(
+          chalk.red.bold(`[err]:${ns}: ${message}`),
+          optionalParams
+        )
+      : error(chalk.red.bold(`[err]:${ns}: ${message}`));
   },
 });
 

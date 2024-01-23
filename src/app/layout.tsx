2e208410644092e49c7ebfddec705d3a6a1fcff1
Frederik Aulich <frederik.aulich@trivago.com>
2024-01-23 01:04:13 +0100

    Migrate to new nextjs directory structure.

---
+++ src/app/layout.tsx
@@ -0,0 +1,24 @@
+import { Providers } from '@app/providers';
+import Body from '@components/Body';
+import { HTML_HEAD_KEYS } from '@config/constants';
+
+import '@styles/globals.css';
+import Head from 'next/head';
+import 'tailwindcss/tailwind.css';
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <Head key={HTML_HEAD_KEYS.LAYOUT}>
+        <link rel="shortcut icon" href="favicons/favicon.ico" />
+      </Head>
+      <Body>
+        <Providers>{children}</Providers>
+      </Body>
+    </html>
+  );
+}

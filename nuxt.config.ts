0b2f4cc56dc35185254ad9a6f46e19e91b4cc505
Frederik Aulich <frederik.aulich@uni-duesseldorf.de>
2026-04-17 23:29:42 +0200

    Replace vite tailwindcss with nuxtjs tailwindcss.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,25 +1,11 @@
-import tailwindcss from '@tailwindcss/vite'
-
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
 
-  modules: ['@nuxt/fonts', '@nuxt/icon', '@nuxt/eslint', '@vueuse/nuxt'],
+  modules: ['@nuxt/fonts', '@nuxt/icon', '@nuxt/eslint', '@nuxtjs/tailwindcss', '@vueuse/nuxt'],
 
   css: ['~/assets/css/main.css'],
-
-  vite: {
-    plugins: [tailwindcss()],
-  },
-
-  devServer: {
-    https: {
-      key: './app/certs/localhost.key',
-      cert: './app/certs/localhost.crt',
-    },
-    port: 3030,
-  },
 
   app: {
     head: {
